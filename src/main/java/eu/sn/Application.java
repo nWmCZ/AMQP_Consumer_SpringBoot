d7f493192d6805bc35df4396bbc609968db19249
Stanislav Novotný <stanislav.novotny@unicorn.com>
2019-10-22 22:41:27 +0200

    Jms listener

--- src/main/java/eu/sn/Application.java
+++ src/main/java/eu/sn/Application.java
@@ -2,10 +2,10 @@ package eu.sn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableJms
 public class Application {
 
     public static void main(String[] args) {
